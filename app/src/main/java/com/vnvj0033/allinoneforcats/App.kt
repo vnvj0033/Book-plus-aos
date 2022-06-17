c0ed0319f1e678f56713eac7d988894d023389a2
vnvj0033 <vnvj0033@gmail.com>
2022-06-18 02:06:59 +0900

    - 파이어베이스 추가 - fcm 노티알림 추가 - SharedPreferences 기반 Data 저장 util 추가

---
+++ app/src/main/java/com/vnvj0033/allinoneforcats/App.kt
@@ -0,0 +1,18 @@
+package com.vnvj0033.allinoneforcats
+
+import android.app.Application
+import com.vnvj0033.allinoneforcats.model.Data
+import com.vnvj0033.allinoneforcats.push.FirebaseMessagingHelper
+
+class App : Application() {
+    companion object {
+        lateinit var data: Data
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        data = Data(applicationContext)
+
+        FirebaseMessagingHelper.updateToken()
+    }
+}
