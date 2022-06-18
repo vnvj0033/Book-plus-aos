737244124c2479ad8b4e60377206d29e70b83ecb
yoosangyeob <syyoo@joara.com>
2022-06-18 18:24:58 +0900

    dagger-hilt 도입

--- app/src/main/java/com/vnvj0033/allinoneforcats/App.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/App.kt
@@ -3,7 +3,9 @@ package com.vnvj0033.allinoneforcats
 import android.app.Application
 import com.vnvj0033.allinoneforcats.model.Data
 import com.vnvj0033.allinoneforcats.push.FirebaseMessagingHelper
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     companion object {
         lateinit var data: Data
