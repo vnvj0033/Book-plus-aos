93bef8e4e3f229961b59c5a17f0e29982abdc23a
yoosangyeob <syyoo@joara.com>
2022-06-18 18:22:23 +0900

    코드 라인 정리

--- app/src/main/java/com/vnvj0033/allinoneforcats/ui/FirstFragment.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/ui/FirstFragment.kt
@@ -15,9 +15,6 @@ import com.vnvj0033.allinoneforcats.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
