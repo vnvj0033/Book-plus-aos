e5e89f087d3c416f986e80653bb0c05faf9cc832
SangYeob <vnvj0033@gmail.com>
2022-06-23 23:08:27 +0900

    - UserProfileEvent 추가 - Data binding 적용

--- app/src/main/java/com/vnvj0033/allinoneforcats/ui/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/ui/userprofile/UserProfileFragment.kt
@@ -1,27 +1,29 @@
-package com.vnvj0033.allinoneforcats.ui
+package com.vnvj0033.allinoneforcats.ui.userprofile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.vnvj0033.allinoneforcats.R
 import com.vnvj0033.allinoneforcats.databinding.FragmentUserProfileBinding
 import com.vnvj0033.allinoneforcats.model.Cat
+import com.vnvj0033.allinoneforcats.model.User
 
 class UserProfileFragment : Fragment(), UserProfileEvent {
 
     private var _binding: FragmentUserProfileBinding? = null
     private val binding get() = _binding!!
 
-    private val catListAdapter = CatListAdapter(this)
+    private val catListAdapter = CatListAdapter(userProfileEvent = this)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_user_profile, container, false)
         return binding.root
     }
 
@@ -30,6 +32,8 @@ class UserProfileFragment : Fragment(), UserProfileEvent {
 
         binding.recyclerviewCatChoices.adapter = catListAdapter
         catListAdapter.addCat(arrayListOf<Cat>().apply { for (i in 0..100) add(Cat()) })
+
+        updateUser(User())
     }
 
     override fun onDestroyView() {
@@ -40,8 +44,9 @@ class UserProfileFragment : Fragment(), UserProfileEvent {
     override fun goToCatDetail(cat: Cat) {
         findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
     }
+
+    override fun updateUser(user: User) {
+        binding.contentUserInfo.user = user
+    }
 }
 
-interface UserProfileEvent{
-    fun goToCatDetail(cat: Cat)
-}
