91bb150e73e94094b767673a527bb07abae42edf
vnvj0033 <vnvj0033@gmail.com>
2022-07-10 18:50:53 +0900

    CatDetailRepository load cat - db network 예시 구현

--- app/src/main/java/com/vnvj0033/allinoneforcats/ui/catdetil/CatDetailRepository.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/ui/catdetil/CatDetailRepository.kt
@@ -6,37 +6,33 @@ import com.vnvj0033.allinoneforcats.db.CatDatabase
 import com.vnvj0033.allinoneforcats.model.Cat
 import com.vnvj0033.allinoneforcats.retrofit.RetrofitCore
 import com.vnvj0033.allinoneforcats.retrofit.requester.CatRequester
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class CatDetailRepository {
 
-    fun loadCat(): Flow<Cat> {
-
+    suspend fun loadCat(): Flow<Cat> = coroutineScope {
         val db = CatDatabase.getInstance(App.context)
 
         val cat = db.catDao().getCat("")
 
-        if (cat != null) return flowOf(cat)
+        db.close()
+
+        if (cat != null) return@coroutineScope flowOf(cat)
 
         val requester = CatRequester.getCat("")
-        RetrofitCore.catApi.getCat(requester).enqueue(object : Callback<Cat> {
-            override fun onResponse(call: Call<Cat>, response: Response<Cat>) {
+        val response = RetrofitCore.catApi.getCat(requester).execute()
 
-            }
-
-            override fun onFailure(call: Call<Cat>, t: Throwable) {
-
-            }
-        })
-
-        return flowOf(
-            Cat(name = App.context.getString(R.string.test_text), description = App.context.getString(R.string.test_text))
-        )
+        if (response.isSuccessful) {
+            return@coroutineScope flowOf(response.body() ?: Cat())
+        } else {
+            return@coroutineScope flowOf(
+                Cat(name = App.context.getString(R.string.test_text), description = App.context.getString(R.string.test_text))
+            )
+        }
     }
+
     fun loadCatList(): Flow<List<Cat>> {
         val list = ArrayList<Cat>().apply { for (i in 0..100) add(Cat()) }
         return flowOf(list)
