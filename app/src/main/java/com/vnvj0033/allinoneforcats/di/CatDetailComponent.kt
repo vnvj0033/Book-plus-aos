6146404db9555a39a0944ea1a139c28e473e0823
SangYeob <vnvj0033@gmail.com>
2022-10-04 21:37:15 +0900

    - use custom Component on CatDetailActivity - fix compatibility of sdk

--- app/src/main/java/com/vnvj0033/allinoneforcats/di/CatDetailComponent.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/di/CatDetailComponent.kt
@@ -1,30 +1,16 @@
 package com.vnvj0033.allinoneforcats.di
 
-import com.vnvj0033.allinoneforcats.ui.catdetil.CatDetailActivity
 import com.vnvj0033.allinoneforcats.ui.catdetil.CatDetailEvent
 import dagger.BindsInstance
-import dagger.Component
 import dagger.hilt.DefineComponent
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.components.SingletonComponent
 
 @DefineComponent(parent = SingletonComponent::class)
 interface CatDetailComponent {
-
-    fun inject(activity: CatDetailActivity)
 
     @DefineComponent.Builder
     interface CatDetailComponentBuilder {
         fun setEvent(@BindsInstance catDetailEvent: CatDetailEvent): CatDetailComponentBuilder
         fun build(): CatDetailComponent
     }
-}
-
-@EntryPoint
-@InstallIn(CatDetailComponent::class)
-interface CatDetailEventEntryPoint {
-    fun inject(): CatDetailActivity
-    fun putEvent(): CatDetailEvent
 }
