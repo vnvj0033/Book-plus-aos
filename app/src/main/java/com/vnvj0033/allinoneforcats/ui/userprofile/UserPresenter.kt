bed768894461e3def63322c7de8b35d702da7554
SangYeob <vnvj0033@gmail.com>
2022-10-11 20:34:27 +0900

    - fix  user inject

--- app/src/main/java/com/vnvj0033/allinoneforcats/ui/userprofile/UserPresenter.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/ui/userprofile/UserPresenter.kt
@@ -3,20 +3,20 @@ package com.vnvj0033.allinoneforcats.ui.userprofile
 import com.vnvj0033.allinoneforcats.model.Cat
 
 class UserPresenter(
-    private val userProfileEvent: UserProfileEvent,
     private val userRepository: UserRepository
 ) {
+    val userProfileEvent: UserProfileEvent? = null
 
     fun goToCatDetail(cat: Cat) {
-        userProfileEvent.goToCatDetail(cat)
+        userProfileEvent?.goToCatDetail(cat)
     }
 
     suspend fun loadData() {
         userRepository.loadCats().collect { cats ->
             userRepository.loadUser(cats).collect { user ->
 
-                userProfileEvent.updateUser(user)
-                userProfileEvent.updateCatList(cats)
+                userProfileEvent?.updateUser(user)
+                userProfileEvent?.updateCatList(cats)
             }
         }
     }
