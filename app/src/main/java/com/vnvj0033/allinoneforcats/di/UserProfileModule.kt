b272fc0745e07fb905f3d16feeb97d0dbb5eb378
SangYeob <vnvj0033@gmail.com>
2022-10-11 20:34:27 +0900

    - fix  user inject

--- app/src/main/java/com/vnvj0033/allinoneforcats/di/UserProfileModule.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/di/UserProfileModule.kt
@@ -2,7 +2,6 @@ package com.vnvj0033.allinoneforcats.di
 
 import com.vnvj0033.allinoneforcats.ui.userprofile.CatListAdapter
 import com.vnvj0033.allinoneforcats.ui.userprofile.UserPresenter
-import com.vnvj0033.allinoneforcats.ui.userprofile.UserProfileEvent
 import com.vnvj0033.allinoneforcats.ui.userprofile.UserRepository
 import dagger.Module
 import dagger.Provides
@@ -13,13 +12,13 @@ import dagger.hilt.android.components.ActivityComponent
 @InstallIn(ActivityComponent::class)
 class UserProfileModule {
 
-//    @Provides
-//    fun providesRepository() = UserRepository()
-//
-//    @Provides
-//    fun providesPresent(event: UserProfileEvent, repository: UserRepository) = UserPresenter(event, repository)
-//
-//    @Provides
-//    fun providesAdapter(presenter: UserPresenter) = CatListAdapter(presenter)
+    @Provides
+    fun providesRepository() = UserRepository()
+
+    @Provides
+    fun providesPresent(repository: UserRepository) = UserPresenter(repository)
+
+    @Provides
+    fun providesAdapter(presenter: UserPresenter) = CatListAdapter(presenter)
 
 }
