d980b4f664180c5ad29059400002a19d91b2b304
yoosangyeob <syyoo@joara.com>
2022-10-20 17:38:13 +0900

    - fixed hilt build error - rename CatDetailRepository -> CatRepository

--- app/src/main/java/com/vnvj0033/allinoneforcats/common/di/CatDetailModule.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/common/di/CatDetailModule.kt
@@ -1,7 +1,8 @@
 package com.vnvj0033.allinoneforcats.common.di
 
 import com.vnvj0033.allinoneforcats.presentation.presenter.CatDetailPresent
-import com.vnvj0033.allinoneforcats.data.repository.CatDetailRepository
+import com.vnvj0033.allinoneforcats.data.repository.CatRepository
+import com.vnvj0033.allinoneforcats.presentation.view.catdetail.CatDetailAdapter
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,10 +13,12 @@ import dagger.hilt.android.components.ActivityComponent
 class CatDetailModule {
 
     @Provides
-    fun providesRepository() = CatDetailRepository()
+    fun providesRepository() = CatRepository()
 
     @Provides
-    fun providesPresent(repository: CatDetailRepository) = CatDetailPresent(repository)
+    fun providesPresent(repository: CatRepository) = CatDetailPresent(repository)
 
+    @Provides
+    fun providesAdapter() = CatDetailAdapter()
 
 }
