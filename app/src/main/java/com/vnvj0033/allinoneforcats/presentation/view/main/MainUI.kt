2a00ebac7339060ef78b80efa76caa808735e6fb
yoosangyeob <syyoo@joara.com>
2022-10-31 19:18:51 +0900

    - fix main ui item click

--- app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/main/MainUI.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/main/MainUI.kt
@@ -8,10 +8,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SnapshotMutationPolicy
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.vnvj0033.allinoneforcats.data.entry.Cat
@@ -34,15 +32,17 @@ private fun CatGrid(state: MainState) {
 
 
         items(state.items) { cat ->
-            CatItem(cat, state.itemClick.value)
+            CatItem(cat) {
+                state.click.value.invoke(cat)
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CatItem(cat: Cat, onClick: (Cat)-> Unit) {
-    Surface(onClick = {onClick.invoke(cat)}) {
+fun CatItem(cat: Cat, click: ()-> Unit) {
+    Surface(onClick = click) {
         AsyncImage(
             model = cat.url,
             contentDescription = "this image is cat item",
@@ -52,5 +52,5 @@ fun CatItem(cat: Cat, onClick: (Cat)-> Unit) {
 
 class MainState {
     val items = mutableStateListOf<Cat>()
-    val itemClick = mutableStateOf({ _: Cat -> })
+    val click = mutableStateOf({_:Cat -> })
 }
