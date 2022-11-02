072d1cedf9014d2f94f82c79b4f7cae5f3e0d212
yoosangyeob <syyoo@joara.com>
2022-11-02 23:13:42 +0900

    - add MainUI, MainState, MainViewModel

--- app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/main/MainActivity.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/main/MainActivity.kt
@@ -1,46 +1,18 @@
 package com.vnvj0033.allinoneforcats.presentation.view.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import com.vnvj0033.allinoneforcats.data.entry.Cat
-
-import com.vnvj0033.allinoneforcats.presentation.presenter.MainViewModel
-import com.vnvj0033.allinoneforcats.presentation.view.detail.CatDetailActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
-    private val mainState = MainState()
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
         setContent {
-            MainUI(mainState)
+            MainUI()
         }
-
-        viewModel.cats.observe(this) {
-            mainState.items.addAll(it)
-        }
-
-        mainState.click.value = {cat: Cat ->
-            val intent = Intent(this, CatDetailActivity::class.java).apply {
-                putExtra("cat", cat)
-            }
-            startActivity(intent)
-        }
-
-        lifecycleScope.launch() {
-            viewModel.loadCatList()
-        }
-
     }
 }
