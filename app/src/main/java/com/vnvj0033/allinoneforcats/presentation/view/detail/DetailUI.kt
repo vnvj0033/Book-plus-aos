0069e9de399e59d776851d47d7460bb529b9539d
yoosangyeob <syyoo@joara.com>
2022-11-04 18:05:12 +0900

    - add detail ui

--- app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/detail/DetailUI.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/detail/DetailUI.kt
@@ -2,10 +2,27 @@ package com.vnvj0033.allinoneforcats.presentation.view.detail
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import coil.compose.AsyncImage
+import com.vnvj0033.allinoneforcats.data.entry.Cat
 
 @Composable
-fun DetailUI() {
-    Row {
+fun DetailUI(viewModel: DetailViewModel) {
+    val state = viewModel.state
+    val cat = state.cat
 
+    Row {
+        AsyncImage(model = cat.url, contentDescription = "this image is cat item" )
     }
+}
+
+class DetailViewModel : ViewModel() {
+    val state = DetailState()
+}
+
+class DetailState {
+    var cat by mutableStateOf(Cat())
 }
