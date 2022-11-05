e7c746c500144f6ae0ffa8d8b9ca5487f6675cc6
vnvj0033 <vnvj0033@gmail.com>
2022-11-05 19:24:56 +0900

    - add hilt module retrofit

--- app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/main/MainActivity.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/presentation/view/main/MainActivity.kt
@@ -3,9 +3,16 @@ package com.vnvj0033.allinoneforcats.presentation.view.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.vnvj0033.allinoneforcats.data.repository.CatRepository
 import com.vnvj0033.allinoneforcats.data.repository.FakeCatRepo
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var catRepository: CatRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
