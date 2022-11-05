4a377bd181f865765ea9489d5f6bfb216f99dbb3
vnvj0033 <vnvj0033@gmail.com>
2022-11-05 20:22:33 +0900

    - add inject FakeCatRepo

--- app/src/main/java/com/vnvj0033/allinoneforcats/data/repository/FakeCatRepo.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/data/repository/FakeCatRepo.kt
@@ -1,8 +1,9 @@
 package com.vnvj0033.allinoneforcats.data.repository
 
 import com.vnvj0033.allinoneforcats.data.entry.Cat
+import javax.inject.Inject
 
-class FakeCatRepo : CatRepository {
+class FakeCatRepo @Inject constructor()  : CatRepository {
     override suspend fun getCatList(): List<Cat> = ArrayList<Cat>().apply {
         add(Cat(url = "https://cdn2.thecatapi.com/images/a7g.jpg"))
         add(Cat(url = "https://cdn2.thecatapi.com/images/a7g.jpg"))
