076c013c0ca58a36175d3fb44b7123108a0d3b7b
yoosangyeob <syyoo@joara.com>
2022-11-27 12:59:42 +0900

    - refactor push notification builder

--- app/src/main/java/com/vnvj0033/allinoneforcats/util/push/CatsFirebaseMessagingService.kt
+++ app/src/main/java/com/vnvj0033/allinoneforcats/util/push/CatsFirebaseMessagingService.kt
@@ -1,21 +1,9 @@
 package com.vnvj0033.allinoneforcats.util.push
 
-import android.annotation.SuppressLint
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.media.RingtoneManager
-import android.os.Build
 import android.util.Log
-import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.vnvj0033.allinoneforcats.presentation.view.main.MainActivity
-import com.vnvj0033.allinoneforcats.R
 
 
 class CatsFirebaseMessagingService : FirebaseMessagingService() {
@@ -38,48 +26,7 @@ class CatsFirebaseMessagingService : FirebaseMessagingService() {
         val title = message.data["title"] ?: ""
         val body = message.data["body"] ?: ""
 
-        sendNotification(title, body)
+        PushNotificationManager().sendNotification(this, title, body)
     }
 
-
-    private fun sendNotification(title: String, body: String) {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // 액티비티 중복 생성 방지
-        }
-
-        val pendingIntent = createPendingIntent(intent)
-
-        val channelId = "DEFAULT_NOTIFICATION_CHANNEL_ID"
-        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION) // 소리
-
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setAutoCancel(true)
-            .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent)
-            .setDefaults(Notification.DEFAULT_SOUND)
-            .build()
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // 오레오 버전 이후에는 채널이 필요
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val channel = NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_HIGH)
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        notificationManager.notify(0, notification)
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
-    private fun createPendingIntent(intent: Intent) =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
-        } else {
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
 }
