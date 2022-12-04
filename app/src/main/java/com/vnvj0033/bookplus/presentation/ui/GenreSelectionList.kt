5b199608b01819b59d786533997a38b622bc8362
yoosangyeob <syyoo@joara.com>
2022-12-04 13:26:19 +0900

    - add GenreSelectionListState

--- app/src/main/java/com/vnvj0033/bookplus/presentation/ui/GenreSelectionList.kt
+++ app/src/main/java/com/vnvj0033/bookplus/presentation/ui/GenreSelectionList.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.vnvj0033.bookplus.presentation.ui.state.GenreSelectionListState
 
 @Composable
 fun GenreSelectionList() {
@@ -26,8 +27,11 @@ fun GenreSelectionList() {
         "취미/생활",
     )
 
+    val state = GenreSelectionListState()
+    state.options.addAll(options)
+
     LazyRow(modifier = Modifier.padding(top = 8.dp)) {
-        items(options) { text ->
+        items(state.options) { text ->
             GenreSelection(text)
         }
     }
---
+++ app/src/main/java/com/vnvj0033/bookplus/presentation/ui/state/GenreSelectionListState.kt
@@ -0,0 +1,7 @@
+package com.vnvj0033.bookplus.presentation.ui.state
+
+import androidx.compose.runtime.mutableStateListOf
+
+class GenreSelectionListState {
+    val options = mutableStateListOf<String>()
+}
