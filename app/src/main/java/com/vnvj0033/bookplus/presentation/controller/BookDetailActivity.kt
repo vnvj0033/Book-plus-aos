be8158ab42b52a9df01fae8d817d4d1fd35c7337
SangYeob <vnvj0033@gmail.com>
2022-12-07 20:14:09 +0900

    - add BookDetailActivity

---
+++ app/src/main/java/com/vnvj0033/bookplus/presentation/controller/BookDetailActivity.kt
@@ -0,0 +1,22 @@
+package com.vnvj0033.bookplus.presentation.controller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.vnvj0033.bookplus.presentation.ui.main.MainUI
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BookDetailActivity : ComponentActivity() {
+    companion object {
+        const val BUNDLE_KEY_NAME_BOOK = "bundle_key_name_book"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MainUI()
+        }
+    }
+}
