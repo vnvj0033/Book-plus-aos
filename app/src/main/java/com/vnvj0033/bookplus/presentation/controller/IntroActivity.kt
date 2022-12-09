fa63746bc4e17049fcd640a168390075a30d8f53
yoosangyeob <syyoo@joara.com>
2022-12-09 11:40:40 +0900

    - add IntroActivity

---
+++ app/src/main/java/com/vnvj0033/bookplus/presentation/controller/IntroActivity.kt
@@ -0,0 +1,64 @@
+package com.vnvj0033.bookplus.presentation.controller
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vnvj0033.bookplus.R
+import com.vnvj0033.bookplus.presentation.ui.AppTheme
+
+
+class IntroActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppTheme {
+                Intro()
+            }
+        }
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 1000)
+    }
+
+    @Composable
+    private fun Intro() {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(128.dp)
+            )
+        }
+
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    private fun Preview() {
+        AppTheme {
+            Intro()
+        }
+    }
+
+}
