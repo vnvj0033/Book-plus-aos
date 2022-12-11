a6cc3c99e2d334dc031342885944ca11e993bd6a
yoosangyeob <syyoo@joara.com>
2022-12-11 13:35:55 +0900

    - add FavoriteGenreCompose title

--- app/src/main/java/com/vnvj0033/bookplus/presentation/ui/FavoriteGenreCompose.kt
+++ app/src/main/java/com/vnvj0033/bookplus/presentation/ui/FavoriteGenreCompose.kt
@@ -1,12 +1,21 @@
 package com.vnvj0033.bookplus.presentation.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FavoriteGenreCompose() {
     Column {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "SUBSCRIPT")
+        Divider()
         BookList()
     }
 }
