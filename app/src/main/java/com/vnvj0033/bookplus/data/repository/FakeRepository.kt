7116f3b485b94d04edeba57dbc048b7733929096
yoosangyeob <syyoo@joara.com>
2022-12-12 23:02:10 +0900

    - fix fakeRepo package

---
+++ app/src/main/java/com/vnvj0033/bookplus/data/repository/FakeRepository.kt
@@ -0,0 +1,21 @@
+package com.vnvj0033.bookplus.data.repository
+
+import com.vnvj0033.bookplus.data.entity.Book
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class FakeBookRepo @Inject constructor(): BookRepository {
+    override suspend fun loadBooks(genre: String): Flow<List<Book>> {
+        return flowOf(listOf( Book()))
+    }
+
+    override suspend fun loadBooks(platform: String, genre: String): Flow<List<Book>> {
+        return flowOf(listOf( Book()))
+    }
+
+    override suspend fun loadGenres(): Flow<List<String>> {
+        val list = listOf("전체", "문학 작품", "국내 소설", "컴퓨터/IT", "취미/생활")
+        return flowOf(list)
+    }
+}
