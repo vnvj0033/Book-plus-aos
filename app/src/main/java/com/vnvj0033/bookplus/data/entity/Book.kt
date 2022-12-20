64d000ef8dc9773659743872d416abc4df26b2a0
SangYeob <vnvj0033@gmail.com>
2022-12-20 21:48:47 +0900

    - add test book data

--- app/src/main/java/com/vnvj0033/bookplus/data/entity/Book.kt
+++ app/src/main/java/com/vnvj0033/bookplus/data/entity/Book.kt
@@ -5,7 +5,7 @@ import androidx.annotation.Keep
 @Keep
 data class Book(
     val imgUtl: String = "",
-    val title: String = "",
-    val writer: String = "",
-    val summary: String = ""
+    val title: String = "title",
+    val writer: String = "writer",
+    val summary: String = "summary"
 )
