3649397d05fe9ad8f052b25b6fd5ab5a8d7b94da
vnvj0033 <vnvj0033@gmail.com>
2023-02-25 22:33:40 +0900

    add selected list on toggle

--- app/src/main/java/com/vnvj0033/bookplus/ui/subscript/SubscriptCompose.kt
+++ app/src/main/java/com/vnvj0033/bookplus/ui/subscript/SubscriptCompose.kt
@@ -3,6 +3,7 @@ package com.vnvj0033.bookplus.ui.subscript
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.vnvj0033.bookplus.ui.AppTheme
@@ -19,14 +20,33 @@ fun SubscriptCompose() {
 @Composable
 private fun SubscriptCompose(name: String) {
 
+    val selectedList = remember {
+        arrayListOf<String>()
+    }
 
     Column(Modifier.fillMaxHeight()) {
-        PlatformSelectionList(
-            state = PlatformsState()) { platform ->
-
+        PlatformSelectionList(PlatformsState()) { platform ->
+            if (selectedList.contains(platform)) {
+                selectedList.remove(platform)
+            } else {
+                selectedList.add(platform)
+            }
         }
 
-        GenreToggleList(options = listOf("abc", "bdds", "abc123", "baasddds", "오나라라", "오나라라ㅏ라", "아주나라라라", "동해물과백두산이마르고닭도록하느님이보우하사우리나라만세무궁화삼천리화려강산", "대한사람 대한으로 길이보전하세"))
+        GenreToggleList(
+            options = listOf(
+                "abc",
+                "bdds",
+                "abc123",
+                "baasddds",
+                "오나라라",
+                "오나라라ㅏ라",
+                "아주나라라라",
+                "동해물과백두산이마르고닭도록하느님이보우하사우리나라만세무궁화삼천리화려강산",
+                "대한사람 대한으로 길이보전하세"
+            ),
+            selectedList = selectedList
+        )
 
     }
 
