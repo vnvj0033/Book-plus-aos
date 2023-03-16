2bc35a837a7ad710fbdc0ed9facb8d1a75741139
yoosangyeob <syyoo@joara.com>
2023-03-16 21:50:58 +0900

    add android.enableJetifier=true in gradle.properties

--- app/src/main/java/com/vnvj0033/bookplus/util/push/PushNotificationManager.kt
+++ app/src/main/java/com/vnvj0033/bookplus/util/push/PushNotificationManager.kt
@@ -1,11 +1,7 @@
 package com.vnvj0033.bookplus.util.push
 
 import android.annotation.SuppressLint
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
