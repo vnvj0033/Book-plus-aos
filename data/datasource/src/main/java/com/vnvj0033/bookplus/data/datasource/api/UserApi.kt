a112164d70a101b9b7193f00b216bd4c55b60500
yoosangyeob <syyoo@joara.com>
2023-03-19 13:01:20 +0900

    add UserApi

---
+++ data/datasource/src/main/java/com/vnvj0033/bookplus/data/datasource/api/UserApi.kt
@@ -0,0 +1,11 @@
+package com.vnvj0033.bookplus.data.datasource.api
+
+import com.vnvj0033.bookplus.data.model.UserInfo
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UserApi {
+
+    @GET("/v1/user")
+    fun loadUerInfo(@Query("user_id") userId: Int): UserInfo
+}
