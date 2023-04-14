e33c6243379c248c17e54b969c38ab066f2e0fc5
yusang-yeob <vnvj0033@gmail.com>
2023-04-14 18:47:54 +0900

    added class of TypeablePreferences

--- data/datasource/src/main/java/com/vnvj0033/bookplus/data/datasource/datastore/PreferenceDatasource.kt
+++ data/datasource/src/main/java/com/vnvj0033/bookplus/data/datasource/datastore/PreferenceDatasource.kt
@@ -1,27 +1,17 @@
 package com.vnvj0033.bookplus.data.datasource.datastore
 
 import android.content.Context
-import androidx.datastore.preferences.core.edit
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.single
 import javax.inject.Inject
 
 class PreferenceDatasource @Inject constructor(
     @ApplicationContext context: Context
 ) {
-    private val datastore = context.appDataStore
-
-    suspend fun loadFcmToken(): String {
-        return datastore.data.map { preferences ->
-            preferences[PreferenceKeys.FCM_TOKEN] ?: ""
-        }.single()
-    }
-
-
+    private var fcmToken = context.typeablePreferences(PreferenceKeys.FCM_TOKEN, "")
+    
+    suspend fun loadFcmToken(): String = fcmToken.loadValue()
+    
     suspend fun saveFcmToken(token: String) {
-        datastore.edit {
-            it[PreferenceKeys.FCM_TOKEN] = token
-        }
+        fcmToken.saveValue(token)
     }
 }
