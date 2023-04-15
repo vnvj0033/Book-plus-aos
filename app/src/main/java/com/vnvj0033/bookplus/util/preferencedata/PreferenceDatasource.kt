d969ce046a9e9bbbc1166f226f3ecbe7ffc1b24f
yusang-yeob <vnvj0033@gmail.com>
2023-04-15 20:18:12 +0900

    Removed unused imports

--- app/src/main/java/com/vnvj0033/bookplus/util/preferencedata/PreferenceDatasource.kt
+++ app/src/main/java/com/vnvj0033/bookplus/util/preferencedata/PreferenceDatasource.kt
@@ -2,10 +2,8 @@ package com.vnvj0033.bookplus.util.preferencedata
 
 import android.content.Context
 import androidx.datastore.preferences.core.edit
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.single
-import javax.inject.Inject
 
 class PreferenceDatasource (context: Context) {
     private val datastore = context.appDataStore
